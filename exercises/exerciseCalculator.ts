470f3d1e9259b57c193d2b26dd21a5063997bd5c
llahti <llahti@student.hive.fi>
2020-07-31 12:27:28 +0300

    Change exerciseCalculator to commandline application

--- exercises/exerciseCalculator.ts
+++ exercises/exerciseCalculator.ts
@@ -1,4 +1,4 @@
-interface ExersiseResult {
+interface ExerciseResult {
   periodLength: number;
   trainingDays: number;
   success: boolean;
@@ -8,7 +8,12 @@ interface ExersiseResult {
   average: number;  
 }
 
-const calculateExcersises = (hours: Array<number>, target: number): ExersiseResult => {
+interface Input {
+  target: number;
+  hours: Array<number>;
+}
+
+const calculateExercises = (hours: Array<number>, target: number): ExerciseResult => {
   const trainingDays = (): number => {
     return hours.reduce((total, h) => h > 0 ? total + 1 : total, 0);
   }
@@ -49,8 +54,26 @@ const calculateExcersises = (hours: Array<number>, target: number): ExersiseResu
     throw new Error('array was empty');
 }
 
+const parseArgs = (args: Array<string>): Input => {
+  const h = [];
+
+  if (args.length < 4) throw new Error('not enough arguments');
+
+  for (let i = 2; i < args.length; i++) {
+    if (isNaN(Number(args[i])))
+      throw new Error('provided values are not numbers')
+    else if (i > 2)
+      h.push(Number(args[i]))
+  }
+  return {
+    target: Number(args[2]),
+    hours: h
+  }
+}
+
 try {
-  console.log(calculateExcersises([], 2));
+  const { target, hours } = parseArgs(process.argv);  
+  console.log(calculateExercises(hours, target));
 } catch (e) {
-  console.log(e.message);
+  console.log('Error:', e.message);
 }
