f24f9298fee67843ec9ff009e073653c96eef505
llahti <llahti@student.hive.fi>
2020-08-19 14:02:43 +0300

    Init patientor-front to git

---
+++ patientor-front/src/PatientPage/index.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import { Container, Icon } from 'semantic-ui-react';
+import { useStateValue } from '../state';
+import { apiBaseUrl } from '../constants';
+import { Patient, Gender } from '../types';
+
+const PatientPage: React.FC = () => {
+  const [{ patients }, dispatch] = useStateValue();
+  const { id } = useParams<{ id: string }>();
+  const patient = Object.values(patients).find(p => p.id === id);
+
+  const findPatient = async (id: string) => {
+    try {
+      const { data: patient } = await axios.get<Patient>(
+        `${apiBaseUrl}/patients/${id}`
+      );
+
+      dispatch({ type: "SET_PATIENT", payload: patient });
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const iconName = (gender: Gender) => {
+    switch (gender) {
+      case 'male':
+        return 'mars';
+      case 'female':
+        return 'venus';
+      case 'other':
+        return 'transgender alternate';
+    }
+  };
+
+  if (id && patient && !patient.ssn)
+    findPatient(id);
+  
+  return patient
+    ? <div>
+        <Container>
+          <h2>{patient.name} <Icon name={iconName(patient.gender)} /></h2> 
+          ssn: {patient.ssn ? patient.ssn : 'loading...'}<br />
+          occupation: {patient.occupation ? patient.occupation : 'loading...'}
+        </Container>
+      </div>
+    : <div>Patient not found</div>;
+};
+
+export default PatientPage;
